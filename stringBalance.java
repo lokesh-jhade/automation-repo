5abd570308c0d2bbf472f0761249f85c006bb8fb
Lokesh Jhade <115209049+lokesh-jhade@users.noreply.github.com>
2025-05-02 09:46:41 +0000

    changes

---
+++ stringBalance.java
@@ -0,0 +1,11 @@
+/*** 
+Balanced Parentheses Problem
+
+Given a string containing parentheses, determine if the string is balanced. A string is considered balanced if every opening parenthesis has a corresponding closing parenthesis of the same type, and vice versa. The parentheses must be properly nested.
+
+Example:
+
+- Input: s = "((()))" Output: true
+- Input: s = "(()" Output: false
+- Input: s = "{[()]}" Output: true
+- Input: s = "{[(]}" Output: false ***/
